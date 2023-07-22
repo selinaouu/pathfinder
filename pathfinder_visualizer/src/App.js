3f8c709e448aa3b6bcb09dc0162ecee6e91dafd9
selinaouu <selinaou8@gmail.com>
2023-07-21 22:46:11 -0500

    first commit

---
+++ pathfinder_visualizer/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PV from './PV/pv';
+
+function App() {
+  return (
+    <div className="App">
+      <PV></PV>
+    </div>
+  );
+}
+
+export default App;
---
+++ pathfinder_visualizer/src/PV/pv.jsx
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import Node from './Node/Node';
+import './pv.css';
+import {dijkstra} from '../algorithms/dijkstras';
+
+export default class PV extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            nodes:[]
+        };
+    }
+    
+    componentDidMount(){
+        const nodes=[];
+        for(let row=0;row<20;row++){
+            const currentRow=[];
+            for(let col=0; col<50;col++){
+                const currentNode={
+                    col,
+                    row,
+                    isStart: row===10 && col===5,
+                    isFinish: row===10 && col===45
+                }
+                currentRow.push(currentNode);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({nodes});
+    }
+    render(){
+        const {nodes}=this.state;        
+        return(
+            <div className='grid'>
+                {nodes.map((row,rowIdx)=>{
+                    return(
+                        <div key={rowIdx}>{row.map((node,nodeIdx)=> {
+                            const{isStart,isFinish}=node;
+                            return(
+                                <Node 
+                                key={nodeIdx}
+                                isStart={isStart}
+                                isFinish={isFinish}
+                                test={'foo'}>
+                                </Node>);
+                        })}
+                    </div>
+                    );
+                })}
+            </div>
+
+        );
+    }
+
+
+}
+
+            
+                    
+
