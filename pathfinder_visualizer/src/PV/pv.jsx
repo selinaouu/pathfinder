7d883261b9d6bb5b544ff3f9aab897e54ecab77f
selinaouu <selinaou8@gmail.com>
2023-07-22 22:23:39 -0500

    updating dijkstras file

--- pathfinder_visualizer/src/PV/pv.jsx
+++ pathfinder_visualizer/src/PV/pv.jsx
@@ -3,57 +3,88 @@ import Node from './Node/Node';
 import './pv.css';
 import {dijkstra} from '../algorithms/dijkstras';
 
+const START_NODE_ROW=10;
+const START_NODE_COL=15;
+const FINISH_NODE_ROW=10;
+const FINISH_ROW_COL=35;
+
+
 export default class PV extends Component{
     constructor(props){
         super(props);
         this.state={
-            nodes:[]
+            grid:[]
         };
     }
     
     componentDidMount(){
-        const nodes=[];
-        for(let row=0;row<20;row++){
-            const currentRow=[];
-            for(let col=0; col<50;col++){
-                const currentNode={
-                    col,
-                    row,
-                    isStart: row===10 && col===5,
-                    isFinish: row===10 && col===45
-                }
-                currentRow.push(currentNode);
-            }
-            nodes.push(currentRow);
-        }
-        this.setState({nodes});
+        const grid=getInitialGrid();
+        this.setState({grid});
+    }
+
+    visualizeDijkstra(){
+        const{grid}=this.state;
+        const startNode=grid[START_NODE_COL][START_NODE_ROW];
+        const finishNode=grid[FINISH_ROW_COL][FINISH_NODE_ROW];
+        const visitedNodesInOrder=dijkstra(grid,startNode,finishNode);
+        this.animateDijkstra(visitedNodesInOrder)
+
     }
     render(){
         const {nodes}=this.state;        
         return(
+            <>
+            <button onClick={()=> this.visualizeDijkstra()}> Visualize Dijkstra's Algo</button>
             <div className='grid'>
                 {nodes.map((row,rowIdx)=>{
                     return(
-                        <div key={rowIdx}>{row.map((node,nodeIdx)=> {
-                            const{isStart,isFinish}=node;
+                        <div key={rowIdx}>
+                            {row.map((node,nodeIdx)=> {
+                            const{row,col,isStart,isFinish}=node;
                             return(
                                 <Node 
                                 key={nodeIdx}
                                 isStart={isStart}
                                 isFinish={isFinish}
-                                test={'foo'}>
+                                test={'foo'}
+                                col={col}
+                                row={row}>
                                 </Node>);
                         })}
                     </div>
                     );
                 })}
             </div>
-
+            </>
+        
         );
     }
-
-
 }
+const getInitialGrid=()=>{
+        const grid=[];
+        for(let row=0;row<20;row++){
+            const currentRow=[];
+            for(let col=0;col<50;col++){
+                currentRow.push(createNode(col,row));
+            }
+            grid.push(currentRow);
+        }
+        return grid;
+};
+const createNode=(col,row)=>{
+    return{
+        col,
+        row,
+        isStart:row===START_NODE_ROW && col===START_NODE_COL,
+        isFinish:row===FINISH_NODE_ROW && col===FINISH_ROW_COL,
+        distance:Infinity,
+        isVisited:false,
+        isWall:false,
+        previousNode:null,
+
+    };
+};
+
 
             
                     
---
+++ pathfinder_visualizer/src/algorithms/dijkstras.js
@@ -0,0 +1,44 @@
+export function dijkstra(grid, startNode, finishNode){
+    const visitedNodesInOrder=[];
+    if(!startNode||!finishNode||startNode===finishNode){
+        return false;
+    }
+    startNode.distance=0;
+    const univisitedNodes=getAllNodes(grid);
+    while(!!univisitedNodes.length){
+        sortNodesByDistance(univisitedNodes);
+        const closestNode = univisitedNodes.unshift();
+        closestNode.isVisited= true;
+        visitedNodesInOrder.push(closestNode);
+        if(closestNode===finishNode) return visitedNodesInOrder;
+        updateNeighbours(closestNode,grid);
+    }
+
+}
+function sortNodesByDistance(univisitedNodes){
+    univisitedNodes.sort((nodeA,nodeB)=> nodeA.distance-nodeB.distance)
+}
+function updateNeighbours(node,grid){
+    const neighbours=getNeighbours(node,grid);
+    for(const neighbour of neighbours){
+        neighbour.distance=node.distance+1;
+    }
+}
+function getNeighbours(node,grid){
+    const neighbours=[]
+    const {col,row} =node;
+    if(row>0) neighbours.push(grid[row-1][col]);
+    if(col>0) neighbours.push(grid[row][col-1]);
+    if(row<grid.length-1)neighbours.push(grid[row+1][col]);
+    if(col<grid[0].length-1) neighbours.push(grid[row][col+1]);
+
+}
+function getAllNodes(grid){
+    const nodes=[];
+    for(const row of grid){
+        for(const node of row){
+            nodes.push(node);
+        }
+    }
+    return nodes;
+}
